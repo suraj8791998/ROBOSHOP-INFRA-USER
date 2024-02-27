7529bb7c752e24dfe1b5defc3797d2a4274a5f38
suraj8791998 <149286300+suraj8791998@users.noreply.github.com>
2024-02-27 19:33:44 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,60 @@
+variable "cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+
+variable "common_tags" {
+  type = map(any)
+  default = {
+    Environment = "Dev"
+    TERRAFORM   = true
+    Project     = "Roboshop"
+  }
+}
+variable "project_name" {
+  default = ""
+}
+
+variable "environment" {
+  default = "dev"
+}
+variable "project_name" {
+  type    = string
+  default = "Roboshop"
+}
+
+
+#THESE VARIABLES ARE FOR PUBLI SUBNET
+variable "public_cidr" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnet" {
+  default = ["public_subnet_1a", "public_subnet_1b"]
+}
+
+variable "azs" {
+   default = ["us-east-1a","us-east-1b"]
+}
+
+
+
+#THESE VARIABLES ARE FOR private SUBNET
+
+variable "private_cidr" {
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+
+variable "private_subnet" {
+  default = ["private_subnet_1a", "private_subnet_1b"]
+}
+
+variable "database_cidr" {
+  default = ["10.0.5.0/24", "10.0.6.0/24"]
+}
+
+
+variable "database_subnet" {
+  default = ["database_subnet_1a", "database_subnet_1b"]
